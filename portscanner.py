d6de9d9d0807af9ed6c389f646a2bd69b186ca0b
guneyceylan <75374206+9ny@users.noreply.github.com>
2021-04-02 02:24:15 +0300

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+
+
+import os
+os.system("apt-get install figlet")
+os.system("clear")
+
+os.system("figlet portscanner")
+
+print(""" 
+made by devilin9
+
+Welcome to Portscanner.
+
+1) Fast port scan
+2) Attempts to determine the version of the service running on port
+3) OS detection 
+4) Open port scan
+""")
+
+islemno = raw_input("Choose an option: ")
+
+
+if(islemno=="1"):
+	hedefip = raw_input("Target IP: ")
+	os.system("nmap" + hedefip)
+elif(islemno=="2"):
+	hedefip = raw_input("Target IP: ")
+	os.system("nmap -sS -sV" + hedefip)
+elif(islemno=="3"):
+	hedefip = raw_input("Target IP: ")
+	os.system("nmap -O" + hedefip)
+elif(islemno=="4"):
+	hedefip = raw_input("Target IP: ")
+	os.system("nmap  --open" + hedefip)
+		
+else:
+	print("Unrecognized option. Portscanner has been closed")
